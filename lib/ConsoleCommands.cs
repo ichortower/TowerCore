1afb379f683392391c3636d9e878a50082399a94
ichortower <ichor@ichortower.com>
2025-12-19 10:29:52 -0500

    Add a tool for doing console commands.

---
+++ lib/ConsoleCommands.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Reflection;
+
+namespace ichortower.TowerCore;
+
+public class ConsoleCommands
+{
+    /*
+     * Search for and apply all declared console command handlers in the given class.
+     * Like with the Harmony system, this relies on an attribute I defined for this
+     * purpose.
+     */
+    public static bool Register(Type t, out int count)
+    {
+        MethodInfo[] funcs = t.GetMethods(BindingFlags.Static | BindingFlags.Public);
+        count = 0;
+        bool ret = true;
+        foreach (var func in funcs) {
+            if (!TryRegisterCommand(func, out string err)) {
+                if (err is not null) {
+                    Log.Error($"Console command registry failed ({func.ReflectedType.FullName}.{func.Name}): " +
+                            err.ToString());
+                    ret = false;
+                }
+                continue;
+            }
+            ++count;
+        }
+        if (!ret) {
+            Log.Error("Some console commands failed to register. Please report this to " +
+                    "ichortower, along with this SMAPI log.");
+        }
+        return ret;
+    }
+
+    internal static bool TryRegisterCommand(MethodInfo func, out string err)
+    {
+        err = null;
+        ConsoleCommand command = (ConsoleCommand)Attribute.GetCustomAttribute(func,
+                typeof(ConsoleCommand));
+        // not having the attribute isn't an error
+        if (command is null) {
+            return false;
+        }
+
+        try {
+            Main.Helper.ConsoleCommands.Add(command.CommandWord, command.HelpText,
+                    (Action<string, string[]>) func.CreateDelegate(typeof(Action<string, string[]>)));
+        }
+        catch (Exception e) {
+            err = e.Message;//$"Method signature not compatible (expected <string, string[]>)";
+            return false;
+        }
+        Log.Trace($"Registered command '{command.CommandWord}' " +
+                $"({func.ReflectedType.FullName}.{func.Name})");
+        return true;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Method)]
+public class ConsoleCommand : Attribute
+{
+    public string CommandWord { get; }
+    public string HelpText { get; }
+
+    public ConsoleCommand(string commandWord, string helpText) {
+        CommandWord = commandWord;
+        HelpText = helpText;
+    }
+}
